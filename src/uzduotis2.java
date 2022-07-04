d8bfb74ab4cb8c7d1f1796794fde80d63e8b0072
audriusxp <palm@inbox.lt>
2022-07-04 13:48:57 +0300

    kitas variantas is kito pc , nezinau kaip toliau

---
+++ src/uzduotis2.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class uzduotis {
+    public static void main(String[] args) {
+        ArrayList<Integer>masyvas= nuskaitytiSkaicius();
+        System.out.println("masyvas = " + masyvas);
+
+        ArrayList<Integer>suma=daugiauDesimt();
+        System.out.println("kiek daugiau desimt = " + suma);
+    }
+
+     public static ArrayList<Integer>nuskaitytiSkaicius(){
+         Scanner skaitytuvas = new Scanner(System.in);
+         ArrayList<Integer> skaiciai= new ArrayList<>();
+         System.out.println("Iveskite keliu skaiciu bus sarasas " + skaiciai);
+         int n=skaitytuvas.nextInt();
+         for (int i = 0; i <n ;i++) {
+             skaiciai.add(skaitytuvas.nextInt());
+         }
+        return skaiciai;
+
+
+    }
+    public static ArrayList<Integer>daugiauDesimt(ArrayList<Integer>skaiciai) {
+        ArrayList<Integer> dydis= new ArrayList<>();
+
+        for (int i = 0; i < skaiciai.size(); i++) {
+            dydis.add..................
+
+        }
+
+    }
+
+
+    }
